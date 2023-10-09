1499a51fbd557f24dbcc55db3d1746d9e676c613
HwangHyoeun <hhe5361@naver.com>
2023-10-10 02:09:30 +0900

    ?

---
+++ trackproject/lib/src/utilities/MyTheme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData MyThemeData() {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+  );
+}
