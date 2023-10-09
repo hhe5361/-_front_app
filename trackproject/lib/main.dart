d38e8e4817a2bfea8c09c6a1f62279e67e9b87d0
HwangHyoeun <hhe5361@naver.com>
2023-10-10 01:16:47 +0900

    init frontend file

--- trackproject/lib/main.dart
+++ trackproject/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trackproject/src/utilities/MyTheme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,16 +7,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const Scaffold());
+    return MaterialApp(title: 'Flutter Demo', theme: MyThemeData());
   }
 }
